190449c25e9d057206ad74180a5ffd72e3ff9e46
HardBoris <bgaibor03@gmail.com>
2022-01-25 22:08:25 -0400

    feat: minus

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const localApi = axios.create({ baseURL: "http://localhost:3335" });
+// export const localApi = axios.create({ baseURL: "http://localhost:3335" });
 
 export const api = axios.create({
   baseURL: "https://kenzie-hamburgueria-bg.herokuapp.com/",
